324b1a2fd8df54c337f715e061ae7d72c8f148b2
Liam Abbott <liam.e.abbott@gmail.com>
2017-09-23 11:33:57 -0400

    removed extra directories

--- ADMIN/test/build_hail.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /Users/labbott/hail
-./gradlew -Dspark.version=2.0.2 createDocs shadowJar archiveZip
-gsutil cp build/distributions/hail-python.zip gs://annotationdb/ADMIN/test/hail-python.zip
-gsutil cp build/libs/hail-all-spark.jar gs://annotationdb/ADMIN/test/hail-all-spark.jar
-
